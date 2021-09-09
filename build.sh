79636ebf50baf8ea09a8e211fc4002b5878679a7
Vince Skahan <vinceskahan@gmail.com>
2021-09-09 16:05:11 -0700

    update with end-to-end working

--- build.sh
+++ build.sh
@@ -2,50 +2,63 @@
 
 repo="weewx.git"
 remote="https://github.com/weewx/${repo}"
+
+# set your desired upstream branch here
 branch="development"
 
 #-----------------------------------------------------------------
 #----- clone or pull upstream repo ---
 #-----------------------------------------------------------------
-#
-# # update checked out repo if present
-# # or do the initial clone if required
-# if [ -d ${repo} ]
-# then
-#   echo "... updating local copy ..."
-#   cd ${repo}
-#   git pull
-#   cd -
-# else
-#   echo "... cloning upstream    ..."
-#   git clone "${remote}" "${repo}"
-# fi
-#
-# # checkout the desired branch
-# cd ./${repo}
-# git checkout ${branch}
-#
+
+# update checked out repo if already present
+# or do the initial clone if required
+if [ -d ${repo} ]
+then
+  echo "... updating local copy ..."
+  cd ${repo}
+  git pull
+  cd -
+else
+  echo "... cloning upstream ${remote} ..."
+  git clone "${remote}" "${repo}"
+fi
+
+# checkout the desired branch
+cd ./${repo}
+git checkout ${branch}
+cd -
 
 #-----------------------------------------------------------------
 #----- build the base os images ----
-#----- note the use of --no-cache to force full rebuilds ---
 #-----------------------------------------------------------------
 
-#  docker build --no-cache -t debian11_build_os --target debian11_build_os -f ./Dockerfile .
-#  docker build --no-cache -t centos8_build_os --target centos8_build_os -f ./Dockerfile .
-#  docker build --no-cache -t leap_build_os --target leap_build_os -f ./Dockerfile .
+# uncomment to rebuild base images from scratch every time
+#    note: opensuse/leap image is 4-5 minutes due to slowness of upstream repo servers
+#
+# this should typically be commented out
+# NOCACHE="--no-cache"
 
-# this one is not needed since deb11 works fine for all debian(ish)
-#  docker build --no-cache -t ubuntu2004_build_os --target ubuntu2004_build_os -f ./Dockerfile .
+docker build ${NOCACHE} -t debian11_build_os --target debian11_build_os -f ./Dockerfile .
+docker build ${NOCACHE} -t centos8_build_os  --target centos8_build_os  -f ./Dockerfile .
+docker build ${NOCACHE} -t leap_build_os     --target leap_build_os     -f ./Dockerfile .
 
-#----- build the actual packages ----
-# temporarily disable rpm signing
-cd "${repo}" && sed -i .bak 's/SIGN=1/SIGN=0/' makefile && cd -
+# noting here for completeness, although not needed since deb11 packages work fine for all debian(ish) os
+#  docker build "${NOCACHE}" -t ubuntu2004_build_os --target ubuntu2004_build_os -f ./Dockerfile .
 
-#docker run -w /mnt/weewx -v `pwd`/weewx.git:/mnt/weewx --rm -it debian11_build_os  make debian-packages
-#docker run -w /mnt/weewx -v `pwd`/weewx.git:/mnt/weewx --rm -it centos8_build_os   make redhat-packages
+#-----------------------------------------------------------------
+#----- build the packages and tarball ----
+#-----------------------------------------------------------------
+
+# temporarily disable rpm signing for this POC
+cd "${repo}" && sed -i .bak 's/SIGN=1/SIGN=0/' makefile && rm makefile.bak && cd -
+
+# run make in an ephemeral container
+docker run -w /mnt/weewx -v `pwd`/weewx.git:/mnt/weewx --rm -it debian11_build_os  make debian-packages
+docker run -w /mnt/weewx -v `pwd`/weewx.git:/mnt/weewx --rm -it centos8_build_os   make redhat-packages
 docker run -w /mnt/weewx -v `pwd`/weewx.git:/mnt/weewx --rm -it leap_build_os      make suse-packages
 
-# reenable disable rpm signing
+# reenable disable rpm signing in makefile
+# so a 'git status' of the sources is clean again
 cd "${repo}" && sed -i .bak 's/SIGN=0/SIGN=1/' makefile
+
 
